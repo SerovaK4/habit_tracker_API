6dcc20d7c38676c5ce6ea2a7d2079ec0e63655a3
ksun <ksusha1354@gmail.com>
2023-09-04 02:17:15 +0300

    Added README.md and fixed some bugs

--- habit/admin.py
+++ habit/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from habit.models import Habit
+
+
+@admin.register(Habit)
+class HabitAdmin(admin.ModelAdmin):
+    list_display = ( 'id', 'user', 'place', 'time', 'action', 'is_pleasant_habit', 'frequency', 'reward', 'time_to_complete', 'is_public', 'pleasant_habit',)
--- habit/tests.py
+++ habit/tests.py
@@ -1,37 +1,42 @@
+from django.urls import reverse
 from rest_framework.test import APITestCase
 from rest_framework import status
 
 from habit.models import Habit
 from users.models import User
 
+from rest_framework.test import APIClient
+from django.test import TestCase
+from django.contrib.auth.models import User
 
-class CourseTestCase(APITestCase):
-
+class MyAPITests(TestCase):
     def setUp(self):
-        self.url = '/v1/habit/'
+        self.client = APIClient()
 
-        self.user = User.objects.create(
-            email='test_1@sky.pro',
-            chat_id=12345,
-        )
-        self.user.set_password('test123')
-        self.user.save()
+        # Создаем тестового пользователя
+        self.user = User.objects.create_user(username='testuser', password='testpass')
+        # Авторизуем пользователя с помощью `client.force_authenticate()`
+        self.client.force_authenticate(user=self.user)
+        # Получаем URL для обращения к API
+        self.url = reverse('my-model-list-api')
+    def test_api_view_without_authentication(self):
+        # Отключаем аутентификацию для тестирования
+        self.client.force_authenticate(user=None)
 
-        self.user_2 = User.objects.create(
-            email='test_2@sky.pro',
-            chat_id=12345,
-        )
-        self.user_2.set_password('test321')
-        self.user_2.save()
+        # Запрос без авторизации
+        response = self.client.get('/my-api/')
+        self.assertEqual(response.status_code, 200)
 
-        self.user_without_tg_chat_id = User.objects.create(
-            email='without_tg_chat@sky.pro',
-        )
+    def test_api_view_with_authentication(self):
+        # Имитация аутентифицированного пользователя
+        self.client.force_authenticate(user=self.user)
 
-        self.user_2.set_password("test321")
-        self.user_2.save()
+        # Запрос с авторизацией
+        response = self.client.get('/my-api/')
+        self.assertEqual(response.status_code, 200)
 
-        self.habit_1 = Habit.objects.create(
+    def test_create_habit(self):
+        habit_1 = Habit.objects.create(
             user=self.user,
             place='testing room',
             time='8:00:00',
@@ -43,62 +48,5 @@ class CourseTestCase(APITestCase):
             is_public=True,
 
         )
-
-        self.habit_2 = Habit.objects.create(
-            user=self.user_2,
-            place='testing room 2',
-            time='16:00:00',
-            action='testing',
-            is_pleasant_habit=False,
-            frequency='1',
-            reward=None,
-            pleasant_habit=None,
-            is_public=False,
-
-        )
-
-        self.habit_3 = Habit.objects.create(
-            user=self.user_2,
-            place='testing room 2',
-            time='16:00:00',
-            action='testing',
-            is_pleasant_habit=True,
-            frequency='1',
-            is_public=True,
-
-        )
-
-
-
-    def test_create_habit(self):
-        habit_1 = {
-            "user": self.user_2,
-            "place": 'testing room 2',
-            "time": '16:00:00',
-            "action": 'testing',
-            "is_pleasant_habit": True,
-            "frequency": '1',
-            "is_public": True,
-        }
-        response = self.client.post(
-            urls="habit/",
-            data=habit_1
-        )
-        self.assertEqual(
-            response.status_code,
-            status.HTTP_201_CREATED
-        )
-
-    def test_get_list(self):
-        """Тестирование вывода списка привычек"""
-
-        self.client.force_authenticate(user=self.user)
-
-        response = self.client.get(
-            'habit/'
-        )
-
-        self.assertEqual(
-            response.status_code,
-            status.HTTP_200_OK
-        )
+        response = self.client.post(self.url, habit_1, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
