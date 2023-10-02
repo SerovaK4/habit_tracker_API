d4ca94245e3a363180bd8563c30e5abfd5075d9b
ksun <ksusha1354@gmail.com>
2023-10-02 18:40:57 +0300

    Fixed problems

--- config/settings.py
+++ config/settings.py
@@ -88,15 +88,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "habit_tracker",
-        "USER": "postgres",
-        "PASSWORD": "mysecretpassword",
-        "HOST": "db",
-        "PORT": 5432
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': "mysecretpassword",
+        'HOST': 'db'
     }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
