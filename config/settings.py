b1d5dba26261d7ddbf085777883d60c9b82acc28
ksun <ksusha1354@gmail.com>
2023-10-05 01:01:31 +0300

    Added 'ENGINE' in DATABASE

--- config/settings.py
+++ config/settings.py
@@ -88,6 +88,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv("DB_NAME"),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
